4b720a719c922b44bafd7e7176977a10671fa763
Opinur Destiana <opinurdestiana@gmail.com>
2023-12-08 19:06:26 +0700

    menambahkan tampilan

---
+++ app/src/main/java/com/example/hikewise/ui/checkup/ImageProcessActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hikewise.ui.checkup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hikewise.R
+
+class ImageProcessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_process)
+    }
+}
